dfda6ff212402003e7038c67567455c8d67d6115
DevAless <devcolona@gmail.com>
2026-05-06 01:33:48 -0500

    Módulo IAM

---
+++ modules/iam/outputs.tf
@@ -0,0 +1,9 @@
+# modules/iam/outputs.tf
+
+output "upload_lambda_role_arn" {
+  value = aws_iam_role.upload_lambda.arn
+}
+
+output "crop_lambda_role_arn" {
+  value = aws_iam_role.crop_lambda.arn
+}
