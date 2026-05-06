fa4dd3817ad81771458212eaf26ec78acc539f60
DevAless <devcolona@gmail.com>
2026-05-06 01:50:46 -0500

    Módulo S3

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,13 @@
+# modules/s3/outputs.tf
+
+output "bucket_id" {
+  value = aws_s3_bucket.images.id
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.images.arn
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.images.bucket
+}
